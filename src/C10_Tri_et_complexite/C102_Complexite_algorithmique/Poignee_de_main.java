45b2a3e52766227950867e3cc38c95e55c1a8119
Rémi Heredero <remi@heredero.ch>
2022-03-07 17:15:30 +0100

    exercice: Poignées de main

--- src/C10_Tri_et_complexite/C102_Complexite_algorithmique/Poignee_de_main.java
+++ src/C10_Tri_et_complexite/C102_Complexite_algorithmique/Poignee_de_main.java
@@ -1,7 +1,24 @@
 package C10_Tri_et_complexite.C102_Complexite_algorithmique;
 
+// Nombre de poignées de mains échangées pour n personnes.
 public class Poignee_de_main {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new Poignee_de_main(10);
     }
+
+    private int n;
+    private long checks = 0;
+
+    Poignee_de_main(int n){
+        this.n = n;
+        compute();
+        System.out.println("Poignées de mains échangée: " + checks);
+    }
+
+	private void compute() {
+        for (int i = n-1; i >= 1; i--) {
+            checks += i;
+        }
+	}
 }
