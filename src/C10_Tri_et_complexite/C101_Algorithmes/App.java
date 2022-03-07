906640c2a7c12e7618031add4de530ee08f34293
Rémi Heredero <remi@heredero.ch>
2022-03-07 07:32:35 +0100

    begining chapter 10

--- src/C10_Tri_et_complexite/C101_Algorithmes/App.java
+++
@@ -1,7 +0,0 @@
-package C10_Tri_et_complexite.C101_Algorithmes;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
---
+++ src/C10_Tri_et_complexite/C101_Algorithmes/Tri.java
@@ -0,0 +1,21 @@
+package C10_Tri_et_complexite.C101_Algorithmes;
+
+// Tri d'un tableau;
+
+public class Tri {
+
+    static void printList(int[] a){
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) throws Exception {
+        int [] a = {4, 7, 1, 3, 6};
+        printList(a);
+        int min = 0;
+        for (int i = min+1; i < a.length; i++) {
+            
+        }
+    }
+}
