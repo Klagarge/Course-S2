2883910eff3df13cb17b6525030317c4b5d7c753
Rémi Heredero <remi@heredero.ch>
2022-03-16 21:08:15 +0100

    chapter 11 done

--- src/C11_Flux_de_donnees/C11a_Les_exceptions/App.java
+++
@@ -1,7 +0,0 @@
-package C11_Flux_de_donnees.C11a_Les_exceptions;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
--- src/C11_Flux_de_donnees/C11b_Lire_et_ecrire_des_fichiers/App.java
+++
@@ -1,9 +0,0 @@
-package C11_Flux_de_donnees.C11b_Lire_et_ecrire_des_fichiers;
-
-// Combien de poignées de mains vont être échangée pour n personne ?
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
